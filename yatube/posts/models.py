622ccc9ed98ac3c0df4fb951abad707ad03a90cb
Sasha Malakhovskii <sky4place@gmail.com>
2022-02-04 18:38:25 +0300

    Review suggestions added and static folder removed from git // Community HW V1.2

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,7 +5,7 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField("group title", max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField()
 
@@ -18,13 +18,15 @@ class Post(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User,
-        on_delete=models.CASCADE,
-        related_name="posts"
+        on_delete=models.CASCADE
     )
     group = models.ForeignKey(
         Group,
         blank=True,
         null=True,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         related_name="posts"
     )
+
+    class Meta:
+        ordering = ["-pub_date"]
