9a40bb5d2d54a1640e14555bae0b7c122b72284f
Sasha Malakhovskii <sky4place@gmail.com>
2022-02-08 08:53:25 +0300

    Users app and logged_out.html template added

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -18,7 +18,8 @@ class Post(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="posts"
     )
     group = models.ForeignKey(
         Group,
@@ -27,6 +28,9 @@ class Post(models.Model):
         on_delete=models.SET_NULL,
         related_name="posts"
     )
+
+    def __str__(self):
+        return self.text
 
     class Meta:
         ordering = ["-pub_date"]
