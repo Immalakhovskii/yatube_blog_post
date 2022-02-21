96ad5db2a094ed03d77b93e69ae93a24bd9f034f
Sasha Malakhovskii <sky4place@gmail.com>
2022-02-21 22:55:26 +0300

    Navigation panel, profile and post_detail views functions added

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import get_object_or_404, render
+from django.contrib.auth import get_user_model
 from django.core.paginator import Paginator
 from .models import Group, Post
+
+User = get_user_model()
 
 
 def index(request):
@@ -27,5 +30,33 @@ def group_posts(request, slug):
         "page_obj": page_obj,
         "group": group,
         "posts": posts,
+    }
+    return render(request, template, context)
+
+
+def profile(request, username):
+    template = "posts/profile.html"
+    user = get_object_or_404(User, username=username)
+    posts = Post.objects.filter(author__username=username)
+    posts_count = Post.objects.filter(author__username=username).count()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context = {
+        "page_obj": page_obj,
+        "posts_count": posts_count,
+        "posts": posts,
+        "user": user,
+    }
+    return render(request, template, context)
+
+
+def post_detail(request, post_id):
+    template = "posts/post_detail.html"
+    post = get_object_or_404(Post, id=post_id)
+    posts_count = Post.objects.filter(author=post.author).count()
+    context = {
+        "posts_count": posts_count,
+        "post": post,
     }
     return render(request, template, context)
--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.urls import path
 
 from . import views
@@ -7,21 +6,4 @@ app_name = "users"
 
 urlpatterns = [
     path("signup/", views.SignUp.as_view(), name="signup"),
-    path(
-        "login/",
-        LoginView.as_view(template_name="users/login.html"),
-        name="login"
-    ),
-    path(
-        "logout/",
-        LogoutView.as_view(template_name="users/logged_out.html"),
-        name="logout"
-    ),
-    path(
-        "password_change/",
-        PasswordChangeView.as_view(
-            template_name="users/password_change_form.html"
-        ),
-        name="password_change"
-    )
 ]
